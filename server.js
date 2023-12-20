49c52810225b14c571986af393dbde2d606b62f3
JP <jonathankbyers@gmail.com>
2023-12-20 11:46:16 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+const rappers = {
+    '21 Savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance the Rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'Dylan': {
+        'age': 25,
+        'birthName': 'Dylan',
+        'birthLocation': 'Atlanta, Georgia'
+    }
+}
+
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response) => {
+    const rappersName = request.params.rapperName
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else response.json(rappers['Dylan'])
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+})
+
